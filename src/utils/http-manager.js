47bdf3ade510b2f55016e6ab6d85f725435f2ec9
danielghandahari <dangha94@gmail.com>
2019-06-09 18:31:50 +0200

    added https in requests

--- src/utils/http-manager.js
+++ src/utils/http-manager.js
@@ -4,7 +4,7 @@ const OPEN_WEATHER_API_KEY = "f0b395ff9e7f2543b078688e23d329e7";
 
 export async function fetchWeather(lat, lon) {
   let jsonRes = await axios.get(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${OPEN_WEATHER_API_KEY}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${OPEN_WEATHER_API_KEY}&units=metric`
   );
 
   return jsonRes.data;
@@ -18,7 +18,7 @@ export async function randomQuote() {
     quote = (await axios({
       method: "GET",
       url:
-        "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback="
+        "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback="
     })).data;
 
     validQuoteFound = quote && quote[0].content.length < 130;
